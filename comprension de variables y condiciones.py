52dceb0788f423275928242e4557cc9b31b0f0ae
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-15 11:05:57 -0500

    Create comprension de variables y condiciones.py

---
+++ comprension de variables y condiciones.py
@@ -0,0 +1,19 @@
+# Rolando Josue Quijije Banchon
+# Software
+# #Tercer semestre
+
+[car for car in['a','e','i','o','u'] if car not in('a','i','o')]
+edad,_peso = 20, 70.5
+nombres = 'Josue Quijije'
+dirDomiciliaria= "Av Milagro, Km26 cdla 12 de octubre"
+Tipo_sexo = 'M'
+civil = True
+usuario = ('jquijije','1234','jquijije2001@gmail.com')
+materias = ['Programacion Web','PHP','POO']
+docente = {'nombre':'Josue','edad':20}
+print("""Mi nombre es {}, tengo {}      años""".format(nombres,edad))
+print(usuario,materias,docente)
+import math
+num1, num2, num, men = 12.572,  15.4,  4,  '1234'
+print(math.ceil(num1), '\t',math.floor(num1))
+print(round(num1,1),'\t',type(num),'\t',type(men))

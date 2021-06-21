78ff1389c1172c29c8eef3f469bb4fcbe0a217b9
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-21 10:33:05 -0500

    Create Lunes 21 de junio 2021.py

---
+++ Lunes 21 de junio 2021.py
@@ -0,0 +1,11 @@
+# Rolando Josue Quijije Banchon
+# Software
+# #Tercer semestre
+
+#Clases del dia Lunes 21/06/2021
+
+class cargo:
+    def __init__(self):
+    self.codigo=1 
+    self.descripcion="Docente"
+    

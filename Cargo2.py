e309fc022ddef353fd4d8dbf27837ca77c794c15
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-21 11:51:07 -0500

    Update Cargo2.py

--- Cargo2.py
+++ Cargo2.py
@@ -10,13 +10,15 @@ class Cargo:
         Cargo.secuencia=Cargo.secuencia+1
         self.codigo=Cargo.secuencia 
         self.descripcion=des
-    
+
+
+
 cargo1= Cargo ()
 print("cargo1",cargo1.codigo,cargo1.descripcion)
 
 cargo2=Cargo("docente")
 print("cargo2",cargo2.codigo,cargo2.descripcion)
 
-cargo3 = Cargo(101)
+cargo3 = Cargo()
 print("cargo3",cargo3.codigo,cargo3.descripcion)
 print(Cargo.secuencia)

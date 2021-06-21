7d90e77ff52b8c08bf3deb198af0d4d5ef999f0c
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-06-21 10:44:15 -0500

    Create estructura de datos lunes 21.py

---
+++ estructura de datos lunes 21.py
@@ -0,0 +1,16 @@
+# Rolando Josue Quijije Banchon
+# Software
+# #Tercer semestre
+
+#Clases del dia Lunes 21/06/2021
+class Cargo:
+    def __init__(self):
+        self.codigo=1 
+        self.descripcion="sin cargo"
+    
+cargo1= Cargo ()
+print(cargo1.codigo,cargo1.descripcion)
+cargo2=Cargo()
+cargo2.codigo=1
+cargo2.descripcion="Ana"
+print(cargo2.codigo,cargo2.descripcion)
